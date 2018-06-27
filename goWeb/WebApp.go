f629e40e64c983b343de18578c0bc6c61efca81c
Karim Gharib <ichkarimgharib2005@gmail.com>
2018-06-28 01:04:24 +0200

    Activate Local Host and Run Html Code

---
+++ goWeb/WebApp.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+func handlerFunc(w http.ResponseWriter, r * http.Request)  {
+
+	fmt.Fprint(w, "<html>" +
+		"<head>" +
+		"<style>" +
+		"h1{" +
+		"color: red;" +
+		"}" +
+		"button{" +
+		"padding: 15px;" +
+		"margin-left: 20px;" +
+		"font-weight: bold" +
+		"}" +
+		"</style>" +
+		"</head>" +
+		"<body>" +
+		"<h1>Welcome In GOLang Server!" +
+		"<button onClick=\"show()\">Click Me</button>" +
+		"<script>" +
+		"function show(){" +
+		"alert('Clicked');" +
+		"}" +
+		"</script>" +
+		"</body>" +
+		"</html> ")
+
+}
+
+func main()  {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000",nil)
+}
