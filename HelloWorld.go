1400326c8a59e5155746922ae51a5d63773ec751
Karim Gharib <ichkarimgharib2005@gmail.com>
2018-06-27 22:06:07 +0200

    first commit

---
+++ HelloWorld.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome In Go!")
+}
---
+++ Syntax.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+//func tryit()  {
+//	fmt.Println("The Root Number 4 is", math.Sqrt(4))
+//}
+func main() {
+	fmt.Println("The Root Number 4 is", math.Sqrt(4))
+	fmt.Print("This is A random Number From(1-100) = ", rand.Intn(100))
+}
