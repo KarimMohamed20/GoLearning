662fcd042fbdeb0e04a468002751f957266a0478
Karim Gharib <ichkarimgharib2005@gmail.com>
2018-06-27 22:59:22 +0200

    Types Updating

--- Types.go
+++ Types.go
@@ -1,5 +1,20 @@
 package main
+import "fmt"
+
+func add(x, y int) int {
+	return  x + y
+}
+
+func multiple(a,b string) (string,string){
+	return a,b
+}
 
 func main() {
-	
+	num1,num2  := 20,20
+	var resultInt = add(num1,num2)
+	fmt.Println(resultInt)
+	fmt.Println(add(num1,num2))
+	w1,w2 := "Hey,","There"
+	fmt.Println(multiple(w1,w2))
+
 }
